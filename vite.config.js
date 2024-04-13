b94b66de1d8451ceebfe4a1cb1a64d88439cc1d1
Sultepronit <sultepronit@gmail.com>
2024-04-13 18:47:35 +0300

    upgraded to PWA

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,51 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        "name": "Learn Japanese",
+        "short_name": "LearnJap",
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#bc002d",
+        "theme_color": "#bc002d",
+        "description": "My app for learning Japanese vocabulary"
+      }
+    })
   ],
   resolve: {
     alias: {
