f73b1b2acd970dd8ca91b05042d283289f8594f4
Sultepronit <sultepronit@gmail.com>
2024-05-06 16:26:18 +0300

    started refactoring table display

--- src/views/CollectedKanjiDb/CollectedKanjiDb.vue
+++ src/views/CollectedKanjiDb/CollectedKanjiDb.vue
@@ -2,18 +2,49 @@
 import { startSession, ready } from './services/crud';
 // import EditSelected from './components/EditSelected.vue';
 import SearchBar from './components/SearchBar.vue';
+import TableRow from './components/TableRow.vue';
 import DbList from './components/DbList.vue';
 
 document.title = 'Collected Kanji DB';
 
+import {
+    displayedRange as range,
+    selectedNumber,
+    select,
+    incrementLastDisplayedRow, // as incrementLastRow,
+    setLastDisplayedRow, // as setLastRow,
+    rowNumber, // as min,
+    viewList,
+    lastDisplayedRow, // as lastRow
+} from './utils/displayAndSelect';
+
 startSession();
-console.log('ready?');
+// console.log('ready?');
 </script>
 
 <template>
     <template v-if="ready">
-        <!-- <EditSelected /> -->
         <SearchBar />
-        <DbList />
+        <DbList 
+            :range="range"
+            :incrementLastRow="incrementLastDisplayedRow"
+            :setLastRow="setLastDisplayedRow"
+            :min="rowNumber"
+            :max="viewList.length"
+            :lastRow="lastDisplayedRow"
+        >
+            <TableRow
+                v-for="row in range"
+                :key="row.id"
+                :row="row"
+                @click="select(row.id)"
+                :class="{selected: selectedNumber === row.id}"
+            />
+            <!-- <p v-slot="props">
+                go!
+                {{ props }}
+            </p> -->
+            <!-- <TableRow v-slot="props" /> -->
+        </DbList>
     </template>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: { enabled: true },
+      // devOptions: { enabled: true },
       manifest: {
         "name": "Learn Japanese",
         "short_name": "LearnJap",
