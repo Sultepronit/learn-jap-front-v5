4d0d585410de1e5fd8210903fbfb89b1b67bf1f8
Sultepronit <sultepronit@gmail.com>
2024-05-07 14:49:17 +0300

    added caching of fonts (?)

--- vite.config.js
+++ vite.config.js
@@ -45,6 +45,38 @@ export default defineConfig({
         "background_color": "#bc002d",
         "theme_color": "#bc002d",
         "description": "My app for learning Japanese vocabulary"
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          }
+        ]
       }
     })
   ],
