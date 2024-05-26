11ebdd7456b77bb0499bf30c25bfe1550f9f8a56
Sultepronit <sultepronit@gmail.com>
2024-05-26 14:19:27 +0300

    removed not effective init of connection to server

--- src/main.js
+++ src/main.js
@@ -3,8 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { get } from '@/services/commonAPI';
-
-get('/init');
-
 createApp(App).mount('#app')
